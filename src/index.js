0846da2a6ac821b7d39da9f98cc39439f4b1dab1
jb00266 <79471463+jb00266@users.noreply.github.com>
2021-03-21 09:18:41 +0000

    Fixed unit conversion
    Fixed units functions and removed misc comments

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 //import "./styles.css";
 
+window.onload = navigator.geolocation.getCurrentPosition(getTemperatureCurrent)
+
 let now = new Date();
 
 function addZero(i) {
@@ -53,84 +55,59 @@ function capitalizeFirstLetter(string) {
 let searchCity = document.querySelector("#search-button");
 searchCity.addEventListener("click", signUpFull);
 
-// function convertToFahrenheit(event) {
-//   event.preventDefault();
-//   let temperatureElement = document.querySelector("#temperature");
-//   let tempC = temperatureElement.innerHTML;
-//   let tempF = (tempC * 9) / 5 + 32;
-//   temperatureElement.innerHTML = Math.round(tempF);
-//   fahrenheitLink.removeEventListener("click", convertToFahrenheit);
-//   celsiusLink.addEventListener("click", convertToCelsius);
-//   celsiusLink.style.opacity = "0.5";
-//   fahrenheitLink.style.color = "white";
-//   fahrenheitLink.style.opacity = "1";
-// }
+function convertToFahrenheit(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  let tempC = temperatureElement.innerHTML;
+  let tempF = (tempC * 9) / 5 + 32;
+  temperatureElement.innerHTML = Math.round(tempF);
+  fahrenheitLink.removeEventListener("click", convertToFahrenheit);
+  celsiusLink.addEventListener("click", convertToCelsius);
+  celsiusLink.style.opacity = "0.5";
+  fahrenheitLink.style.cursor = "auto"
+  fahrenheitLink.style.color = "white";
+  fahrenheitLink.style.opacity = "1";
+  celsiusLink.style.cursor = "pointer"
+}
 
-// function convertToCelsius(event) {
-//   event.preventDefault();
-//   let temperatureElement = document.querySelector("#temperature");
-//   let tempF = temperatureElement.innerHTML;
-//   let tempC = (tempF - 32) * 0.5556;
-//   temperatureElement.innerHTML = Math.round(tempC);
-//   celsiusLink.removeEventListener("click", convertToCelsius);
-//   fahrenheitLink.addEventListener("click", convertToFahrenheit);
-//   fahrenheitLink.style.opacity = "0.5";
-//   celsiusLink.style.color = "white";
-//   celsiusLink.style.opacity = "1";
-// }
+function convertToCelsius(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  let tempF = temperatureElement.innerHTML;
+  let tempC = (tempF - 32) * 0.5556;
+  temperatureElement.innerHTML = Math.round(tempC);
+  celsiusLink.removeEventListener("click", convertToCelsius);
+  fahrenheitLink.addEventListener("click", convertToFahrenheit);
+  fahrenheitLink.style.opacity = "0.5";
+  fahrenheitLink.style.cursor = "pointer"
+  celsiusLink.style.color = "white";
+  celsiusLink.style.cursor = "auto"
+  celsiusLink.style.opacity = "1";
+}
 
-// function convertToCelsius2(event) {
-//   event.preventDefault();
-//   let temperatureElement2 = document.querySelector("#temperature2");
-//   let temp2F = temperatureElement2.innerHTML;
-//   let temp2C = (temp2F - 32) * 0.5556;
-//   temperatureElement2.innerHTML = Math.round(temp2C);
-//   celsiusLink2.removeEventListener("click", convertToCelsius2);
-//   fahrenheitLink2.addEventListener("click", convertToFahrenheit2);
-//   fahrenheitLink2.style.opacity = "0.5";
-//   celsiusLink2.style.color = "white";
-//   celsiusLink2.style.opacity = "1";
-// }
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", convertToFahrenheit);
 
-// function convertToFahrenheit2(event) {
-//   event.preventDefault();
-//   let temperatureElement2 = document.querySelector("#temperature2");
-//   let temp2C = temperatureElement2.innerHTML;
-//   let temp2F = (temp2C * 9) / 5 + 32;
-//   temperatureElement2.innerHTML = Math.round(temp2F);
-//   fahrenheitLink2.removeEventListener("click", convertToFahrenheit2);
-//   celsiusLink2.addEventListener("click", convertToCelsius2);
-//   celsiusLink2.style.opacity = "0.5";
-//   fahrenheitLink2.style.color = "white";
-//   fahrenheitLink2.style.opacity = "1";
-// }
-
-//let fahrenheitLink = document.querySelector("#fahrenheit-link");
-//fahrenheitLink.addEventListener("click", convertToFahrenheit);
-
-//let celsiusLink = document.querySelector("#celsius-link");
-//celsiusLink.addEventListener("click", convertToCelsius);
-
-//let fahrenheitLink2 = document.querySelector("#fahrenheit-link2");
-//fahrenheitLink2.addEventListener("click", convertToFahrenheit2);
-
-//let celsiusLink2 = document.querySelector("#celsius-link2");
-//celsiusLink2.addEventListener("click", convertToCelsius2);
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", convertToCelsius);
 
 function showWeather(response) {
-  console.log("show wearther");
   let temperatureElement = document.querySelector("#temperature");
   let iconElement = document.querySelector("#icon");
   let temperature = Math.round(response.data.main.temp);
   let weathericon = response.data.weather[0].icon;
   let windspeed = response.data.wind.speed;
   let windtext = document.querySelector("#windspeed");
-  console.log(windspeed);
   let humidity = response.data.main.humidity;
   let humiditytext = document.querySelector("#humidity");
-  console.log(humidity);
   temperatureElement.innerHTML = temperature;
-  //fahrenheitLink.style.opacity = "0.5";
+  celsiusLink.removeEventListener("click", convertToCelsius);
+  fahrenheitLink.addEventListener("click", convertToFahrenheit);
+  fahrenheitLink.style.opacity = "0.5";
+  celsiusLink.style.cursor = "auto"
+  fahrenheitLink.style.cursor = "pointer"
+  celsiusLink.style.color = "white";
+  celsiusLink.style.opacity = "1";
   iconElement.setAttribute(
     "src",
     `http://openweathermap.org/img/wn/${weathericon}@2x.png`
@@ -140,7 +117,6 @@ function showWeather(response) {
 }
 
 function userLocation() {
-  console.log("userlocation");
   navigator.geolocation.getCurrentPosition(getTemperatureCurrent);
 }
 
@@ -193,11 +169,19 @@ function getTemperature(position) {
 }
 
 function getTemperatureCurrent(position) {
-  console.log("testing1");
   let apiKey = "2a2ce705840d65130b449fa1016ec67f";
   let lat = position.coords.latitude;
   let lon = position.coords.longitude;
-  console.log("testing2");
   let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
   axios.get(url).then(showWeather);
+axios.get(url).then(showLocation);
+  url = `https://api.openweathermap.org/data/2.5/forecast/?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
+  axios.get(url).then(showForecast);
 }
+
+function showLocation(response) {
+ let userCity = response.data.name;
+  let heading = document.querySelector("#maintitle");
+ heading.innerHTML = "The weather in " + userCity;
+}
+
